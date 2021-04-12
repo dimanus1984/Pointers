00e4477dc70b821630e41311e131e620b3f45642
Dima Nuss <dimanus1984@gmail.com>
2021-04-12 16:42:32 +0400

    initOne

--- DynamicMemory/Arrays1.h
+++ DynamicMemory/Arrays1.h
@@ -1,4 +1,5 @@
 #pragma once
+//Прототипы функций выносятся в заголовочные файлы (*.h)
 void push_back(int*& arr, int& n, int value);
 void push_front(int*& arr, int& n, int value);
 void insert(int*& arr, int& n, int value, int index);
